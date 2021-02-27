5e10ae163b67e253f425810b5e3cce7777540e8a
synapseindiaprojects <projects@synapseindia.com>
2021-02-27 18:16:46 +0530

    Adding config file
    Config file for defining constants

---
+++ ffmpeg-config.php
@@ -0,0 +1,4 @@
+<?php
+define("DEFAULT_FFMPEG_PATH", "/usr/bin/ffmpeg"); // set this path according to server environment
+define("DEFAULT_FFPROBE_PATH", "/usr/bin/ffprobe"); // set this path according to server environment
+define("FFMPEG_WRAPPER_DEBUG_PRINTS", true);  // set this to true to show debug messages otherwise set it to false
